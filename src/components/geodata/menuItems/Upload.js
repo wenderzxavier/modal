df3f375a4ce541828b3917c581089fefcfddaf8f
Wender Xavier <wender.zacariasxavier@gmail.com>
2018-11-05 16:39:10 -0200

    feat: indexed database
    start implementing indexed database, inserting upload file data to idb.

--- src/components/geodata/menuItems/Upload.js
+++ src/components/geodata/menuItems/Upload.js
@@ -8,9 +8,28 @@ class Upload extends Component {
         fileSize: ''
     }
 
+    deleteDB(dbName){
+        window.indexedDB.deleteDatabase('openModal')
+    }
+
+    populateDB(evt) {
+        evt.preventDefault()
+        const indexedDB = window.indexedDB ||    // Use the standard DB API
+            window.mozIndexedDB ||             // Or Firefox's early version of it
+            window.webkitIndexedDB;            // Or Chrome's early version   
+        const IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+        const IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
+        const dbVersion = 1
+        this.deleteDB('openModal')
+        const req = indexedDB.open('openModal', dbVersion, {storage: 'temporary'})
+        req.onerror = () => console.log('Database failed to open')
+        req.onsuccess = () => console.log('Database opened successfully');
+        let db = req.result
+    }
+
+
     previewData() {
         let file = $('#input-data')[0].files[0]
-        console.log(file)
         this.setState({
             fileName: file.name,
             fileSize: file.size
@@ -20,9 +39,13 @@ class Upload extends Component {
 
     render() {
         const { fileName, fileSize } = this.state
+        const indexedDB = window.indexedDB ||    // Use the standard DB API
+            window.mozIndexedDB ||             // Or Firefox's early version of it
+            window.webkitIndexedDB;            // Or Chrome's early version
         return (
             <div className="menu-content">
-                <form action="/">
+                {indexedDB ? (
+                    <form action="/">
                     <div className='input-area menu-content'>
                         <label htmlFor="input-data" className="input-item">
                             <i className="fas fa-file-upload fa-lg"></i>Upload File
@@ -35,12 +58,13 @@ class Upload extends Component {
                                 <p>File: {fileName}</p>
                                 <p>Size: {fileSize / 1000}KB</p>
                             </div>
-                            <div>
-                                <input id='submit-data' type='submit' value='Submit' name='submit'></input>
+                            <div id='submitBtn'>
+                                <input id='submit-data' type='submit' value='Submit' name='submit' onClick={(evt) => this.populateDB(evt)}></input>
                             </div>
                         </div>
                     }
                 </form>
+                ) : <p>Your browser does not support indexedDB. Please, try again using another browser.</p>}
             </div>
         )
     }
