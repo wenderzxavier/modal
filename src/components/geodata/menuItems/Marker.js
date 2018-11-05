47b0cea497f9ca5b5dff85d26cb4a4f40cdf8dde
Wender Xavier <wender.zacariasxavier@gmail.com>
2018-11-05 14:40:46 -0200

    style: add style to marker menu section
    alter icons sizes. Add marker color input button. Need to fix button
    logic

--- src/components/geodata/menuItems/Marker.js
+++ src/components/geodata/menuItems/Marker.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import MarkerData from '../../../utils/MarkerData'
-import '../../../styles/MenuItems.css'
+import React from "react";
+import MarkerData from "../../../utils/MarkerData";
+import $ from 'jquery'
+import "../../../styles/MenuItems.css";
+
+const changeColor = () => {
+    let newColor = $('#markerColor').val()
+    console.log(newColor)
+    $('.marker-type svg').css('fill', newColor)
+}
 
 const Marker = () => (
-    <div className='menu-content menu-wrap'>
-        {MarkerData.map((marker, key) => (
-            <div><label key={key} className='marker-type'>{marker.icon}<p>{marker.name}</p></label></div>
-        ))}
+  <div>
+    <div className="menu-content menu-wrap">
+      {MarkerData.map((marker, key) => (
+        <label key={key} className="marker-type">
+          {marker.icon}
+        </label>
+      ))}
     </div>
-)
+    <div className="menu-content">
+      <input
+        id="markerColor"
+        type="color"
+        name="markerColor"
+        defaultValue="#ffffff"
+        onChange={changeColor}
+      />
+      <p>Select Marker Color</p>
+    </div>
+  </div>
+);
 
-export default Marker
+export default Marker;
